7470d6e4705746731564c151ba1a78cae9e302c3
VolksRat71 <nathanieljryan1994@gmail.com>
2019-09-03 19:07:36 -0700

    application is capturing input value and creating a child in another container using correct syntax

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -9,16 +9,18 @@ import {
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText)
   };
 
   const addGoalHandler = () => {
-    console.log(enteredGoal);
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
   }
 
   return (
+    // Text Input and Button
     <View style={styles.screen}>
       <View style={styles.container}>
         <TextInput
@@ -29,11 +31,16 @@ export default function App() {
         />
         <Button
           title="ADD"
+          style={styles.button}
           onPress={addGoalHandler}
         />
       </View>
+      {/* Upon Render, empty container, onPress list */}
       <View>
-
+        {courseGoals.map((goal) =>
+          <View key={goal} style={styles.listItem}>
+            <Text>{goal}</Text>
+          </View>)}
       </View>
     </View>
   );
@@ -46,11 +53,24 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   textInput: {
     borderColor: 'black',
     borderWidth: 1,
-    width: '80%'
+    borderRadius: 5,
+    width: '80%',
+    padding: 5,
   },
+  button: {
+
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1,
+  }
 });
